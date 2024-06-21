3ddd37805428d49c8657a1b7464325161bfde3ad
Diego Medina <diegocab456@gmail.com>
2024-06-21 15:23:38 -0300

    creacion de 'executeMethod' para invocar un metodo de forma dinamica

--- src/main/java/com/dynamic/factory/ApiClient.java
+++ src/main/java/com/dynamic/factory/ApiClient.java
@@ -1,11 +1,5 @@
 package com.dynamic.factory;
 
-import java.util.List;
-import java.util.Map;
-
 public interface ApiClient {
-    String getHola();
-    List<Map<String, Object>> getPersonas();
-
     String executeMethod(String method);
 }
